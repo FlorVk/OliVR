edb24dc5d1be20c79b5dd2a2407d96689b098aff
FlorVk <florvankuyk@hotmail.com>
2022-12-01 10:15:59 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landingspagina</title>
+    <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <nav>
+        <div class="nav__block">
+            <a href="">Product naam</a>
+            <a class="btn" href="portaal.php">Portaal login</a>
+        </div>
+        
+    </nav>
+    
+
+    <main>
+        <section class="section section--highlight section__flex">
+            <div class="text__left">
+                <h1>Tot rust komen op eigen wijze</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus feugiat molestie erat, id tristique tellus consequat non. Praesent volutpat dolor purus, non congue metus tristique non. Cras nec efficitur nisl. Morbi vitae diam et massa placerat pulvinar. Sed maximus tellus sed enim efficitur, vitae tempor ante auctor. </p>
+            </div>
+            <img class="intro__image image" src="images/placeholder.jpg" alt="">
+        </section>
+
+        <section class="section">
+            <h2>Wat probleem pakken we aan?</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum arcu sed mauris auctor, sit amet faucibus leo euismod. Fusce nisi mi, finibus eu vehicula sollicitudin, ornare non elit. Vestibulum tempus viverra ex, ut ullamcorper felis viverra nec. Etiam efficitur facilisis nisl, sed malesuada magna. Vivamus faucibus mauris id suscipit laoreet. Vestibulum facilisis efficitur purus. Nunc ullamcorper auctor nisl, eu pulvinar est egestas eget. Donec porta bibendum lectus, eget mollis ipsum scelerisque vitae. Vivamus sem nisi, viverra sed condimentum ut, interdum eu enim. <br> Nullam lobortis mattis sem sed tincidunt. Praesent id ipsum dapibus, sodales metus et, euismod turpis. Pellentesque aliquam nulla non dui rutrum pharetra ut vitae erat. Quisque auctor a orci id venenatis. Aliquam hendrerit suscipit magna quis commodo. Morbi bibendum, purus vitae fringilla viverra, nisl diam volutpat quam, pretium lobortis nunc erat ut ligula. Donec tincidunt, arcu vel posuere blandit, lectus lectus blandit leo, ac bibendum arcu enim nec erat. Integer volutpat, enim a ullamcorper pharetra, urna massa consequat ipsum, at molestie urna justo porttitor lacus.</p>
+        </section>
+
+        <section class="section section--highlight">
+            <h2>wat bieden we aan?</h2>
+            <div class="block__flex">
+                <div>
+                    <img class="icon" src="images/vr-glasses.png" alt="">
+                    <h3>VR-pakket</h3>
+                </div>
+
+                <div>
+                    <img class="icon" src="images/monitor.png" alt="">
+                    <h3>Platform</h3>
+                </div>
+
+                <div>
+                    <img class="icon" src="images/dashboard.png" alt="">
+                    <h3>Dashboard</h3>
+                </div>
+            </div>
+        </section>
+
+        <section class="section section__flex">
+            <img class="intro__image image" src="images/placeholder.jpg" alt="">
+            <div class="text__right">
+                <h1>Bekijk ons prijzenpakket</h1>
+                <p>placeholder </p>
+                <a class="btn" href="">Vraag aan</a>
+            </div>
+            
+        </section>
+    </main>
+
+    <footer>
+        <div class="footer__block">
+
+        </div>
+    </footer>
+</body>
+</html>
