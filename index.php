6acb02c2e781393e246e7de8f1b3c30167058029
FlorVk <florvankuyk@hotmail.com>
2022-12-01 14:31:17 +0100

    add: body text + styling

--- index.php
+++ index.php
@@ -9,31 +9,44 @@
     <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-    <nav>
-        <div class="nav__block">
+    <div class="nav">
+        <nav class="nav__block">
             <a href="">Product naam</a>
             <a class="btn" href="portaal.php">Portaal login</a>
+        </nav>
+        <div class="circle__nav">
+            <div class="circle circle__xl circle__one"></div>
+            <div class="circle circle__s circle__two"></div>
+            <div class="circle circle__xs circle__three"></div>
         </div>
-        
-    </nav>
+</div>
     
 
     <main>
-        <section class="section section--highlight section__flex">
-            <div class="text__left">
+        <section class="section section--highlight flex padding__x__xxl padding__y__xl">
+            <div class="padding__right">
                 <h1>Tot rust komen op eigen wijze</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus feugiat molestie erat, id tristique tellus consequat non. Praesent volutpat dolor purus, non congue metus tristique non. Cras nec efficitur nisl. Morbi vitae diam et massa placerat pulvinar. Sed maximus tellus sed enim efficitur, vitae tempor ante auctor. </p>
+                <p class="padding__x__xs">Dagelijks ervaren we vele prikkels. Met al deze prikkels omgaan, is niet altijd voor iedereen even makkelijk. Hiervoor bedachten wij ... (naam vh project) ... . ... is een concept waarbij we een pakket willen aanbieden aan scholen. </p>
+                <p>Middelbare schoolstudenten hebben natuurlijk een druk programma. Het concept zou een extra tool kunnen zijn bij de studiebegeleiding in reguliere scholen, maar kan ook als extra ondersteuning in time-out klassen in het buitengewoon onderwijs.
+</p>
             </div>
             <img class="intro__image image" src="images/placeholder.jpg" alt="">
         </section>
 
         <section class="section">
             <h2>Wat probleem pakken we aan?</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum arcu sed mauris auctor, sit amet faucibus leo euismod. Fusce nisi mi, finibus eu vehicula sollicitudin, ornare non elit. Vestibulum tempus viverra ex, ut ullamcorper felis viverra nec. Etiam efficitur facilisis nisl, sed malesuada magna. Vivamus faucibus mauris id suscipit laoreet. Vestibulum facilisis efficitur purus. Nunc ullamcorper auctor nisl, eu pulvinar est egestas eget. Donec porta bibendum lectus, eget mollis ipsum scelerisque vitae. Vivamus sem nisi, viverra sed condimentum ut, interdum eu enim. <br> Nullam lobortis mattis sem sed tincidunt. Praesent id ipsum dapibus, sodales metus et, euismod turpis. Pellentesque aliquam nulla non dui rutrum pharetra ut vitae erat. Quisque auctor a orci id venenatis. Aliquam hendrerit suscipit magna quis commodo. Morbi bibendum, purus vitae fringilla viverra, nisl diam volutpat quam, pretium lobortis nunc erat ut ligula. Donec tincidunt, arcu vel posuere blandit, lectus lectus blandit leo, ac bibendum arcu enim nec erat. Integer volutpat, enim a ullamcorper pharetra, urna massa consequat ipsum, at molestie urna justo porttitor lacus.</p>
+            <h3>De VR-ruimte</h3>
+                <p class="padding__x__xs">Aan de hand van een interactieve VR-ruimte willen we kinderen in de middelbare school de kans geven om even te ontsnappen aan alle prikkels die zij ervaren. Er zullen enkele virtuele ruimtes ter beschikking gesteld worden waarvan de leerlingen gebruik kunnen maken.</p>
+                <p>Deze ruimtes zijn vrij uiteenlopend. Zo is het de bedoeling dat je in de ene volledig tot ontspanning kan komen, maar in een andere je net kan afreageren.</p>
+                <p>De ruimtes zijn dus specifiek voor de leerlingen ontworpen en in samenspraak met enkele middelbare studenten hebben we deze uitgebouwd. Zo konden we het makkelijkste op hun vragen inspelen.</p>
+            <h3>Het dashboard</h3>
+                <p>Het dashboard is gekoppeld aan de virtuele ruimtes. Deze moet fungeren als informatiebron voor (zorg)leerkrachten, begeleiders en eventueel psychologen.</p>
+                <p>Wanneer de leerling in de virtuele ruimte aangeeft waarom hij/zij even nood had om te ontsnappen aan alle prikkels, kan de leerkracht nadien in het dashboard nagaan wat de reden was. Hierdoor is het makkelijker in te spelen op specifieke noden van individuele leerkrachten.</p>
         </section>
 
+
         <section class="section section--highlight">
-            <h2>wat bieden we aan?</h2>
+            <h2>Wat bieden we aan?</h2>
             <div class="block__flex">
                 <div>
                     <img class="icon" src="images/vr-glasses.png" alt="">
@@ -52,14 +65,37 @@
             </div>
         </section>
 
-        <section class="section section__flex">
-            <img class="intro__image image" src="images/placeholder.jpg" alt="">
-            <div class="text__right">
-                <h1>Bekijk ons prijzenpakket</h1>
-                <p>placeholder </p>
-                <a class="btn" href="">Vraag aan</a>
+        <section class="section">
+        <h2>Bekijk ons prijzenpakket</h2>
+            <div class="block__flex">
+                <img class="intro__image image" src="images/placeholder.jpg" alt="">
+                <div class="flex__text padding__x__xs">
+                    <p class="padding__x__xs">Het gehele concept bevat 3 dingen: een VR-bril, de VR-ruimtes en het dashboard.</p>
+                    <p>De VR-bril dient eenmalig aangekocht te worden. De VR-ruimtes en het dashboard kan als pakket gekocht worden. Dit zal in abbonementvorm verkrijgbaar te zijn. Elk schooljaar zal de prijs herzien worden naar de vraag van de school.</p>
+                    <a class="btn" href="">Vraag aan</a>
+                </div>
             </div>
             
+        </section>
+
+        <section class="section">
+            <h2>Wie zijn wij?</h2>
+            <p>Wij zijn 3 enthousiaste studenten Interactive Multimedia Design aan de Thomas More hogeschool. Voor ons eindwerk onderzochten we verschillende problematieken in het mentale welzijn. Dit is een heel breed onderwerp en na vele gesprekken in verschillende gebieden, kwamen we terecht bij ons concept nu: virtuele ruimtes voor middelbare studenten waar ze kunnen ontsnappen aan te veel prikkels.</p>
+            <p>Ons idee was om dit concept uit te breiden, specifiek voor kinderen met ASS. Omdat dit niet zo eenvoudig ligt, houden we onze doelgroep momenteel heel breed en op deze manier kunnen alle scholen hier gebruik van maken.</p>
+            <div class="block__flex padding__top">
+                <div>
+                    <img class="profile__image" src="images/placeholder.jpg" alt="">
+                    <p>Naam</p>
+                </div>
+                <div>
+                    <img class="profile__image" src="images/placeholder.jpg" alt="">
+                    <p>Naam</p>
+                </div>
+                <div>
+                    <img class="profile__image" src="images/placeholder.jpg" alt="">
+                    <p>Naam</p>
+                </div>
+            </div>
         </section>
     </main>
 
